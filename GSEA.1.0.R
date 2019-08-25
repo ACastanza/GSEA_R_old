892c072fbc94462f9d6a2b29c5e4b25279114c56
Alex Wenzel <atwenzel@eng.ucsd.edu>
2019-08-24 20:40:35 -0700

    manually added change from base commit e6b6f030

--- GSEA.1.0.R
+++ GSEA.1.0.R
@@ -930,7 +930,7 @@ GSEA <- function(
     rm(temp)
   } else  if (gene.ann == "") {
     for (i in 1:N) {
-      all.gene.descs[i] <- gene.map[2,i]
+      all.gene.descs[i] <- gene.map[i,2]
       all.gene.symbols[i] <- gene.labels[i]
     }
   }
